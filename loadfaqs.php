889d4f678bf38e05e486056de499bc6fa069a1d6
unknown <shahzeb.naqvi12@gmail.com>
2022-03-31 22:12:35 +0500

    faqs and video api

---
+++ loadfaqs.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+include('include/User.php');
+$user = new User();
+// $_GET['service_id'] = 1;
+$sql = "SELECT `id`, `question`, `answer`, `time` FROM `faqs`";
+$result = mysqli_query($user->dbConnect,$sql);
+$row = mysqli_fetch_all($result,MYSQLI_ASSOC);
+echo json_encode($row);
+
+?>
