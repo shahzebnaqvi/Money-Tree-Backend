a12a69fb0effa3b73ad6f7e23177fb17896e1dbc
unknown <shahzeb.naqvi12@gmail.com>
2022-04-06 16:13:17 +0500

    api plan details

--- planhistory.php
+++ planhistory.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 include('include/User.php');
 $user = new User();
-$sql = "SELECT plans.`id`, plans.`plan_amount`, plans.`plan_mode` ,plan_user.`user_id`, plan_user.`type`, plan_user.`status`, plan_user.`amount`, plan_user.`create_date` FROM `plans` ,`plan_user` where `plans`.id = `plan_user`.plan_id AND `user_id`=".$_GET['service_id']."";
+$sql = "SELECT  plans.`plan_amount`, plans.`plan_mode` ,plan_user.`user_id`, plan_user.`type`, plan_user.`status`, plan_user.`amount`, plan_user.`create_date` FROM `plans` ,`plan_user` where `plans`.id = `plan_user`.plan_id AND `user_id`=".$_GET['usernum']."";
 $result = mysqli_query($user->dbConnect,$sql);
 $row = mysqli_fetch_all($result,MYSQLI_ASSOC);
 echo json_encode($row);
