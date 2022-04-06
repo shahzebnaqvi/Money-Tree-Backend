3d606784ec8b03085037f9b402f3b3daf165cfc9
unknown <shahzeb.naqvi12@gmail.com>
2022-04-06 16:04:35 +0500

    api plan details

--- loadblogs.php
+++ loadblogs.php
@@ -3,9 +3,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 include('include/User.php');
 $user = new User();
-$sql = "SELECT `id`, `blog_title`, `blog_long_discript`, `blog_short_discript`, `blog_image` FROM `blog` order by `id` desc";
+$sql = "SELECT  `title`, `link`, `time` FROM `youtubelinks`";
 $result = mysqli_query($user->dbConnect,$sql);
 $row = mysqli_fetch_all($result,MYSQLI_ASSOC);
 echo json_encode($row);
-
 ?>
