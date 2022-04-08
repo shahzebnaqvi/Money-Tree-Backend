ede1c61ea0f72c2b56cc3fce4ecd44164b64501e
unknown <shahzeb.naqvi12@gmail.com>
2022-04-08 17:02:57 +0500

    upload image

--- histwallet.php
+++ histwallet.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 include('include/User.php');
 $user = new User();
-$sql = "SELECT `tranaction_type`, `time`, `amount`, `image`, `status` FROM `wallet` WHERE `user_id` =".$_GET['usernum']." And `tranaction_type`='withdraw' OR `tranaction_type`='credit'";
+$sql = "SELECT `tranaction_type`, `time`, `amount`, `image`, `status` FROM `wallet` WHERE `user_id` =".$_GET['usernum']." And (`tranaction_type`='withdraw' OR `tranaction_type`='credit')";
 $result = mysqli_query($user->dbConnect,$sql);
 $row = mysqli_fetch_all($result,MYSQLI_ASSOC);
 echo json_encode($row);
