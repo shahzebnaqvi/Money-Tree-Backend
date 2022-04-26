a7be926f42fbb1f4cc2a2d7c578f1dbaabc81efa
unknown <shahzeb.naqvi12@gmail.com>
2022-04-26 16:13:23 +0500

    api

--- profitposting.php
+++ profitposting.php
@@ -7,7 +7,7 @@ $user = new User();
 $sql = "SELECT `id`, `plan_id`, `date`, sum(`profit_percent`)'profit' FROM `plan_posting` where `plan_posting`.`plan_id` = 1 group by `date` order by `plan_posting`.`date` ";
 $result = mysqli_query($user->dbConnect,$sql);
 // echo $sql;
-$main = 100;
+$main = 0;
 $year_array = [];
 
 for($k=6;$k>0;$k--){
@@ -25,13 +25,30 @@ $yearly = [];
 $j=0;
 $k=0;
 for($i=0;$i<sizeof($row);$i++){
-    $main = $main + $row[$i]['profit'];
-    $weekly[date('d',strtotime($row[$i]['date']))] = $main;
+    $main =  $row[$i]['profit'];
+    $monthly[date('m',strtotime($row[$i]['date']))] = $main;
+    // $sql = "SELECT `id`, `plan_id`, `date`, sum(`profit_percent`)'profit' FROM `plan_posting` where `plan_posting`.`plan_id` = 1 group by `date` order by `plan_posting`.`date` where `date` >= and `date`<=  ";
+    $result = mysqli_query($user->dbConnect,$sql);
+    $aq = $sql;
+
     // array_push($weekly, $main);
     // array_push($weekly, $main);
 }
+
+
+for($i=0;$i<sizeof($row);$i++){
+    $main = $row[$i]['profit'];
+    $weekly[date('d',strtotime($row[$i]['date']))] = $main;
+  $result = mysqli_query($user->dbConnect,$sql);
+
+}
 // echo "<pre>";
 // print_r($response);
-echo json_encode($weekly);
+
+ $weekly =array_slice($weekly,-7,7,true);
+    $size = sizeof($weekly);
+      echo $weekly;
+  
+echo json_encode(array('weekly'=>$weekly,'monthly'=>$monthly,'yearly'=>$yearly));
 // echo "</pre>";
 ?>
